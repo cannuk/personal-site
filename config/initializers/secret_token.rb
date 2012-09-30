03b3b947a1f2befc62ecf9f9e1a36f6a4cae9c93
Sean Neden <sean.neden@pipewise.com>
2012-09-30 15:49:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalSite::Application.config.secret_token = '20a1ddf1f2eaa488238ca1e14bc3388c03c674ac4419428e7eca8f7003dc70c8c4f6aed5164db567359c3ba989893f82672fe199e3a4aeb45372336705954760'
